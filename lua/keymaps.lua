0a2416e6ec95083a78ea85aefd55f78d2976f749
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-01 00:57:19 +0200

    Added last keymap to make nvim colemak-compatible

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,7 +2,6 @@ vim.keymap.set("", "<Space>", "<Leader>", opts)
 
 vim.keymap.set("", "m", "h", opts) vim.keymap.set("", "M", "H", opts)
 vim.keymap.set("", "n", "j", opts) vim.keymap.set("", "N", "J", opts)
-
 vim.keymap.set("", "e", "k", opts) vim.keymap.set("", "E", "K", opts)
 vim.keymap.set("", "i", "l", opts) vim.keymap.set("", "I", "L", opts)
 
@@ -14,7 +13,26 @@ vim.keymap.set("", "l", "u", opts) vim.keymap.set("", "L", "U", opts)
 vim.keymap.set("", "j", "y", opts) vim.keymap.set("", "J", "Y", opts)
 vim.keymap.set("", ";", "p", opts) vim.keymap.set("", ":", "P", opts)
 
-vim.keymap.set("", "o", ";", opts) vim.keymap.set("", "O", ":<Ignore>", opts)
+vim.keymap.set("", "k", "n", opts) vim.keymap.set("", "K", "N", opts)
+
+vim.keymap.set("", "h", "m", opts) vim.keymap.set("", "H", "M", opts)
+
+vim.keymap.set("", "s", "d", opts) vim.keymap.set("", "S", "D", opts)
+vim.keymap.set("", "c", "x", opts) vim.keymap.set("", "C", "X", opts)
+
+vim.keymap.set("", "p", "r", opts) vim.keymap.set("", "P", "R", opts)
+vim.keymap.set("", "r", "s", opts) vim.keymap.set("", "R", "S", opts)
+vim.keymap.set("", "d", "c", opts) vim.keymap.set("", "D", "C", opts)
+
+vim.keymap.set("", "z", "b", opts) vim.keymap.set("", "Z", "B", opts)
+vim.keymap.set("", "f", "e", opts) vim.keymap.set("", "F", "E", opts)
+
+vim.keymap.set("", "t", "f", opts) vim.keymap.set("", "T", "F", opts)
+vim.keymap.set("", "b", "t", opts) vim.keymap.set("", "B", "T", opts)
+
+vim.keymap.set("", "x", "z", opts) vim.keymap.set("", "X", "Z", opts)
+
+vim.keymap.set("", "o", ";", opts) vim.keymap.set("", "O", ":", opts)
 --[[
 local opts = {
     noremap = true,
