ceb5acfc8a43569c0a01b36995d04f04e75f1d35
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-03 16:55:01 +0200

    Set up cmp plugin for completion

--- lua/plugin_setups/lsp_zero.lua
+++ lua/plugin_setups/lsp_zero.lua
@@ -12,3 +12,37 @@ require("mason-lspconfig").setup
         lsp_zero.default_setup,
     }
 })
+
+vim.keymap.set("i", "<C-e>", "<Nop>")
+
+local cmp = require("cmp")
+cmp.setup({
+  sources = {
+    {name = "nvim_lsp"},
+  },
+  mapping = {
+    ["<C-o>"] = cmp.mapping.confirm({select = false}),
+    ["<C-h>"] = cmp.mapping.abort(),
+    -- ["<C-e>"] = cmp.mapping.select_prev_item({behavior = "select"}),
+    -- ["<C-n>"] = cmp.mapping.select_next_item({behavior = "select"}),
+    ["<C-e>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = "insert"})
+      else
+        cmp.complete()
+      end
+    end),
+    ["<C-n>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = "insert"})
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+})
