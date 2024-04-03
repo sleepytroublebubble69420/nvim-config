fae4f9592fd23c75e7b1e15226f41062f374137e
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-04 01:06:17 +0200

    Installed venv selector plugin

--- lua/setup_lazy.lua
+++ lua/setup_lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -29,16 +29,16 @@ require("lazy").setup
     },
     {
         "nyoom-engineering/oxocarbon.nvim"
-        -- Add in any other configuration; 
-        --   event = foo, 
+        -- Add in any other configuration;
+        --   event = foo,
         --   config = bar
         --   end,
     },
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
+    { "ellisonleao/gruvbox.nvim",            priority = 1000,  config = true },
     { "sainnhe/gruvbox-material" },
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl",     opts = {} },
     {
-        "nvim-treesitter/nvim-treesitter", 
+        "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
             local configs = require("nvim-treesitter.configs")
@@ -59,9 +59,25 @@ require("lazy").setup
 
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim" },
-    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+    { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
     { "neovim/nvim-lspconfig" },
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/nvim-cmp" },
-    { "L3MON4D3/LuaSnip" }
+    { "L3MON4D3/LuaSnip" },
+    {
+        'linux-cultist/venv-selector.nvim',
+        dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+        opts = {
+            -- Your options go here
+            name = "venv",
+            auto_refresh = true
+        },
+        event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+        keys = {
+            -- Keymap to open VenvSelector to pick a venv.
+            { '<leader>vs', '<cmd>VenvSelect<cr>' },
+            -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+            { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+        },
+    }
 })
